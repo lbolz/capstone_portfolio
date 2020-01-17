4b05e9fc8df654ecc69182366a57b3775c9db45c
Lena Bolz <bolz.lena@gmail.com>
2020-01-17 17:00:58 -0500

    made all page urls

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),        
+    path('', views.home, name="home"),
+    path('creativity', views.creativity, name="creativity"),
+    path('critical_thinking', views.critical_thinking, name="critical_thinking"),
+    path('career_management', views.career_management, name="career_management"),
+    path('time_management', views.time_management, name="time_management"),
+    path('professionalism', views.professionalism, name="professionalism"),
+    path('reflection', views.reflection, name="reflection"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "pages/home.html", {})
+
+def creativity(request):
+    return render(request, "pages/creativity.html", {})
+
+def critical_thinking(request):
+    return render(request, "pages/critical_thinking.html", {})
+
+def career_management(request):
+    return render(request, "pages/career_management.html", {})
+
+def time_management(request):
+    return render(request, "pages/time_management.html", {})
+
+def professionalism(request):
+    return render(request, "pages/professionalism.html", {})
+
+def reflection(request):
+    return render(request, "pages/reflection.html", {})
